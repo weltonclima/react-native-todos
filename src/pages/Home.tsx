280039ac729b015ed0c98aefb617039dd0128711
Welton Lima <wlima@epharma.com.br>
2022-08-30 02:14:05 -0300

    Finalizando desafio

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,27 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    setTasks(old => [...old, {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false
+    }]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const data = tasks.map(t => t);
+    const FindTaks = data.find(f => f.id == id);
+
+    if (!FindTaks) return
+
+    FindTaks.done = !FindTaks.done;
+
+    setTasks(data);
+
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(old => old.filter(f => f.id !== id));
   }
 
   return (
@@ -26,10 +38,10 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
   )
